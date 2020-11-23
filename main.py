708e3abdab45dd4cb8fc3fa1942634d1648fcd66
WirKekc <bk9419@yandex.ru>
2020-11-23 11:56:30 +0300

    Update main.py
    Исправил rezult на result
    Привел в более приличный вид в соответствии с PEP8
    Добавил комментарии

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import random
-class Card():
+
+
+class Card:                                 # класс карты
     def __init__(self, name, suit, score):
         self.name = name
         self.suit = suit
         self.score = score
 
-    def show(self):
+    def show(self):                         # Вывод имени и масти карты
         print(self.name, self.suit)
 
 
-class Deck():
-    def __init__(self,key=''):
+class Deck:
+    def __init__(self, key=''):             # По умолчанию колода пуста
 
         self.deck = []
-        suits = ['Червей','Бубей','Пик','Треф']
-        names = {'Туз':11,
-                 'Король':4,
-                'Дама':3,
-                'Валет':2,
-                '10':10,
-                '9':9,
-                '8':8,
-                '7':7,
-                '6':6,
-                '5':5,
-                '4':4,
-                '3':3,
-                '2':2,
-                'Джокер':0,
+        suits = ['Червей', 'Бубей', 'Пик', 'Треф']
+        names = {'Туз': 11,
+                 'Король': 4,
+                 'Дама': 3,
+                 'Валет': 2,
+                 '10': 10,
+                 '9': 9,
+                 '8': 8,
+                 '7': 7,
+                 '6': 6,
+                 '5': 5,
+                 '4': 4,
+                 '3': 3,
+                 '2': 2,
+                 'Джокер': 0,
         }
         if key == 36:
             for suit in suits:
@@ -36,53 +38,47 @@ class Deck():
                     if key == '6':
                         break
 
-
-
-
-    def show(self):
+    def show(self):                         # Вывод колоды
         for card in self.deck:
             card.show()
 
-
-    def shuffle(self):
+    def shuffle(self):                      # Перемешивание колоды
         random.shuffle(self.deck)
 
-    def pick_top_card(self):
+    def pick_top_card(self):                # Берем верхнюю карту колоды
         top_card = self.deck.pop()
         return top_card
 
-    def count_score(self):
+    def count_score(self):                  # Считаем очки
         score = 0
         for card in self.deck:
-            score+=card.score
+            score += card.score
 
         return score
 
 
-
-
-
 def player_play():
-    hand = Deck()
-    deck1 = Deck(36)
-    deck1.shuffle()
-    hand.deck.append(deck1.pick_top_card())
-    hand.deck.append(deck1.pick_top_card())
-    hand.show()
+    hand = Deck()                           # Создаем пустую руку
+    deck1 = Deck(36)                        # Создаем колоду в 36 карт
+    deck1.shuffle()                         # Перемешиваем
+    hand.deck.append(deck1.pick_top_card())     # Берем первую карту из колоды
+    hand.deck.append(deck1.pick_top_card())     # Берем вторую карту из колоды
+    hand.show()                             # Вывод карт в руке
     while True:
-        score = int(hand.count_score())
+        score = int(hand.count_score())     # Считаем очки
         print('Очков у вас в руке {}'.format(score))
-        if score > 21:
+        if score > 21:                      # Если больше 21, то проигрышь
             print('Перебор(')
             break
-        vvod = input('Хочешь еще 1 карту?(y/n): ')
+        vvod = input('Хочешь еще 1 карту?(y/n): ')  # Берем или не берем еще одну карту
         if vvod == 'y':
             hand.deck.append(deck1.pick_top_card())
         if vvod == 'n':
-            print('Набратно очков: {}. Ждем результат соперника'.format(score))
+            print('Набратно очков: {}. Ждем результат соперника'.format(score))     # Вывод резултатов
             break
     return score
 
-rezult = player_play()
-print(rezult)
+
+result = player_play()
+print(result)
 
