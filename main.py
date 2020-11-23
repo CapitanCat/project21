a3de401c446556b27c085efd34dbcc5515004b61
Степа! <skysoldier95@mail.ru>
2020-11-24 01:50:46 +0300

    Кое-что переименовал в имевшемся коде, скажем, в классе Deck поле deck->cards, так оно и интуитивней, и конструкций типа deck1.deck.append возникнуть не может Добавил класс Player и большой метод для сражения двух ИИ game_template. Пока сражаются клоны, но это несложно исправить

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Card:                                 # класс карты
 class Deck:
     def __init__(self, key=''):             # По умолчанию колода пуста
 
-        self.deck = []
+        self.cards = []
         suits = ['Червей', 'Бубей', 'Пик', 'Треф']
         names = {'Туз': 11,
                  'Король': 4,
@@ -34,53 +34,99 @@ class Deck:
         if key == 36:
             for suit in suits:
                 for key, value in names.items():
-                    self.deck.append(Card(key,suit,value))
+                    self.cards.append(Card(key, suit, value))
                     if key == '6':
                         break
 
     def show(self):                         # Вывод колоды
-        for card in self.deck:
+        for card in self.cards:
             card.show()
 
     def shuffle(self):                      # Перемешивание колоды
-        random.shuffle(self.deck)
+        random.shuffle(self.cards)
 
     def pick_top_card(self):                # Берем верхнюю карту колоды
-        top_card = self.deck.pop()
+        top_card = self.cards.pop()
         return top_card
 
     def count_score(self):                  # Считаем очки
         score = 0
-        for card in self.deck:
-            score += card.score
-
+        if self.cards:
+            for card in self.cards:
+                score += card.score
         return score
+
+
+class Player:
+    def __init__(self):
+        self.hand = Deck()
+        self.passed = False
+
+    def makemove(self):
+        if self.hand.count_score() >= 17:  # Ставим некоторое условие, при котором должны пасовать:
+            self.passed = True
 
 
 def player_play():
     hand = Deck()                           # Создаем пустую руку
-    deck1 = Deck(36)                        # Создаем колоду в 36 карт
-    deck1.shuffle()                         # Перемешиваем
-    hand.deck.append(deck1.pick_top_card())     # Берем первую карту из колоды
-    hand.deck.append(deck1.pick_top_card())     # Берем вторую карту из колоды
-    hand.show()                             # Вывод карт в руке
+    deck = Deck(36)                        # Создаем колоду в 36 карт
+    deck.shuffle()                         # Перемешиваем
+    hand.cards.append(deck.pick_top_card())     # Берем первую карту из колоды
+    hand.cards.append(deck.pick_top_card())     # Берем вторую карту из колоды
     while True:
+        hand.show()  # Вывод карт в руке
         score = int(hand.count_score())     # Считаем очки
         print('Очков у вас в руке {}'.format(score))
-        if score > 21:                      # Если больше 21, то проигрышь
+        if score > 21:                      # Если больше 21, то проигрыш
             print('Перебор(')
             break
         vvod = input('Хочешь еще 1 карту?(y/n): ')  # Берем или не берем еще одну карту
         if vvod == 'y':
-            hand.deck.append(deck1.pick_top_card())
+            hand.cards.append(deck.pick_top_card())
         if vvod == 'n':
             print('Набратно очков: {}. Ждем результат соперника'.format(score))     # Вывод резултатов
             break
     return score
 
 
+def game_template():
+    player1 = Player()  # Сейчас сражаются клоны, потом каждый подставит свой класс такого шаблона
+    player2 = Player()
+    for p in player1, player2:  # Выполняем следующий код для обоих игроков
+        deck = Deck(36)
+        deck.shuffle()
+        p.hand.cards.append(deck.pick_top_card())
+        p.hand.cards.append(deck.pick_top_card())
+        p.makemove()  # Этот метод некоторым образом решает, пасовать или нет; если нет, то тянем карту
+        while True:
+            p.makemove()  # Этот метод некоторым образом решает, пасовать или нет; если нет, то тянем карту
+            # Игрок тянет карты или пока не спасует сам, или пока не наберет больше 21
+            if p.hand.count_score() > 21 or p.passed is True:
+                break
+            p.hand.cards.append(deck.pick_top_card())  # Если не спасовали, то тянем карту
+        if p.hand.count_score() > 21:  # Если набрали больше 21, то буквально сбрасываем руку:
+            p.hand = Deck()
+            p.passed = True  # И на всякий случай принудительно становимся спасовавшим
+
+    score1 = player1.hand.count_score()
+    score2 = player2.hand.count_score()
+    if score1 > score2:
+        # print('Со счетом ' + str(score1) + ' : ' + str(score2) + ' победил первый игрок!')
+        return 'Первый игрок победил'
+    elif score1 < score2:
+        # print('Со счетом ' + str(score1) + ' : ' + str(score2) + ' победил второй игрок!')
+        return 'Второй игрок победил'
+    else:
+        # print('Со счетом ' + str(score1) + ' : ' + str(score2) + ' матч окончился вничью!')
+        return 'Ничья'
+
+
 if __name__ == "__main__":      # Чтобы при импорте файла main не начинала выполняться программа
-    result = player_play()
-    print(result)
+    results = {'Первый игрок победил': 0, 'Второй игрок победил': 0, 'Ничья': 0}
+    for i in range(20000):
+        results[game_template()] += 1
+    print(*results.items())
+    #result = player_play()
+    #print(result)
 
 
